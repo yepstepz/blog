1ef182419a224b4ae774fc2b358c9db4d65e8686
yepstepz <prettyoptimist@gmail.com>
2020-11-07 01:31:01 +0300

    fix(podcasts): style snippets

--- src/templates/single/Podcast.js
+++ src/templates/single/Podcast.js
@@ -1,30 +1,36 @@
 import React from "react"
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
 
 import { Layout } from '../../components/layout'
 import { ArticleTile } from '../../components/article-tile'
-import { Title } from '../../components/partials/title'
-import { LG } from '../../utils/constants'
-import { ColumnStyled } from '../../components/partials/common.styles'
+import { LAYOUT_POST_PAGE, LG } from '../../utils/constants'
 import { PODCASTS_TYPE } from '../../utils/constants'
-import { EpisodeNumberStyled } from '../../components/partials/common.styles'
+import { divider } from '../../components/partials/common.styles'
 
 export default ({ data }) => {
     const {
         podcastEpisode
     } = data
     return (
-        <Layout>
-            <Title size={LG}>{ podcastEpisode.title }</Title>
-            <ColumnStyled
-                css={css`
-                    margin-bottom: 100px;
-                `}
-            >
-                <div dangerouslySetInnerHTML={{ __html: podcastEpisode.longDescription}} />
-                <ArticleTile css={css` margin-top: 90px;`} {...podcastEpisode} articleTileSize={LG} postType={PODCASTS_TYPE} hideCategory hideTitle />
-            </ColumnStyled>
+        <Layout layoutType={LAYOUT_POST_PAGE}>
+            <h1>{ podcastEpisode.title }</h1>
+                <ArticleTile
+                    {...podcastEpisode}
+                    articleTileSize={LG}
+                    postType={PODCASTS_TYPE}
+                    hideCategory
+                    hideTitle
+                />
+                <div
+                    css={(theme) => css`
+                        &:not(:last-child) {
+                            margin: 40px 0;
+                            ${divider(theme)};
+                        }
+                    `}
+                />
+                <div className="content" dangerouslySetInnerHTML={{ __html: podcastEpisode.longDescription}} />
         </Layout>
     );
 }
