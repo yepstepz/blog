9f4acdb00c42a5f352b8659765c916720dfbe748
yepstepz <prettyoptimist@gmail.com>
2023-11-06 13:24:10 +0300

    fix toggler

--- components/Partials/ThemeSwitcher/index.jsx
+++ components/Partials/ThemeSwitcher/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import cn from "classnames";
 import styles from './ThemeSwitcher.module.css';
 
 import { ThemeProviderContext } from "@components/ThemeProvider";
@@ -7,13 +8,18 @@ export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useContext(ThemeProviderContext);
 
   return (
-    <label className={styles.Toggler}>
-      <input
-        className={styles.Checkbox}
-        type="checkbox"
-        onChange={toggleTheme}
-        checked={theme === 'dark'}
-      />
-    </label>
+    <div className={styles.TogglerWrapper}>
+      <label className={cn(styles.Toggler, theme === 'dark' ? styles.TogglerActive : '')}>
+        <input
+          className={styles.Checkbox}
+          type="checkbox"
+          onChange={toggleTheme}
+          checked={theme === 'dark'}
+        />
+      </label>
+      <span className={styles.TogglerEmotion}>
+        {theme === 'dark' ? '🌚' : '🌞'}
+      </span>
+    </div>
   )
 }
