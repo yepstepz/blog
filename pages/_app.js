b95829fd6dcdfc5419a4d22d19a22cee437cf4fe
yepstepz <prettyoptimist@gmail.com>
2023-11-13 18:41:18 +0300

    add new post
    change about-me
    
    fix dev analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '@styles/globals.css'
 import { Analytics } from '@components/Partials/Analytics'
 
+const isProduction = process.env.NODE_ENV === "production";
+
 function Application({ Component, pageProps }) {
   return (
     <>
-      <Analytics/>
+      { isProduction ? <Analytics/> : null }
       <Component {...pageProps} />
     </>
   )
