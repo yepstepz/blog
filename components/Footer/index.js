35d8fdc6d1445c0ef1dd954686d49bfc24b9a003
yepstepz <prettyoptimist@gmail.com>
2023-11-17 08:00:00 +0300

    feat(rss): add icon
    add rel me

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
                         href="https://github.com/yepstepz" 
                         target="_blank"
                         title="Мой гитхаб"
-                        rel="noopener noreferrer"
+                        rel="me authn"
                     >
                         <svg viewBox="0 0 24 24">
                             <path
@@ -33,6 +33,17 @@ export default function Footer() {
                                 d="M23.91 3.79L20.3 20.84c-.25 1.21-.98 1.5-2 .94l-5.5-4.07-2.66 2.57c-.3.3-.55.56-1.1.56-.72 0-.6-.27-.84-.95L6.3 13.7.85 12c-1.18-.35-1.19-1.16.26-1.75l21.26-8.2c.97-.43 1.9.24 1.53 1.73z">
                             </path>
                         </svg>
+                    </a>
+                </li>
+                <li className={styles.socials__item}>
+                    <a 
+                        className={styles.socials__icon}
+                        href="https://yepstepz.io/rss.xml" 
+                        title="Rss фид"
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <img className={styles.icon_img} src="/rss-logo.png" />
                     </a>
                 </li>
             </ul>
