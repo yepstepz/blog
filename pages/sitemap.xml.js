2a29c9533e9690a490ff1836fa6b23c8672857a2
yepstepz <prettyoptimist@gmail.com>
2023-11-19 13:40:13 +0300

    fix(*): add lastmod to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,11 +5,12 @@ function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      ${posts
-      .map(({ slug }) => {
+      .map(({ slug, date, lastEdit }) => {
         const url = 'https://yepstepz.io/' + path.join('posts', slug)
         return `
        <url>
            <loc>${url}</loc>
+           <lastmod>${new Date(lastEdit || date).toISOString()}</lastmod>
        </url>
      `;
       })
@@ -25,6 +26,8 @@ function SiteMap() {
 export async function getServerSideProps({ res }) {
   const posts = getAllPosts([
     "slug",
+    "date",
+    "lastEdit"
   ]);
 
   // We generate the XML sitemap with the posts data
