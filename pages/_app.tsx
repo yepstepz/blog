9327b094c06b5d879329ab901dc3039056d620fd
yepstepz <prettyoptimist@gmail.com>
2023-12-31 13:43:27 +0300

    try w/o analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,14 @@
+import '@styles/fonts.css';
 import '@styles/globals.css';
+import '@styles/code/theme.css';
 import { Analytics } from '@components/Partials/Analytics';
 import type { AppProps } from 'next/app';
-import localFont from 'next/font/local';
-
-// Font files can be colocated inside of `app`
-const Inter = localFont({
-  src: [
-    {
-      path: '../fonts/Inter-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Inter-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Inter-SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Inter-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-});
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 function Application({ Component, pageProps }: AppProps) {
   return (
-    <div className={Inter.className}>
+    <div>
       {isProduction ? <Analytics /> : null}
       <Component {...pageProps} />
     </div>
