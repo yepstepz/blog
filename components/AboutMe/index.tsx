e41e2f9c6c7ffaac074d4e261748783fe619bc30
yepstepz <prettyoptimist@gmail.com>
2024-01-12 18:24:54 +0300

    feat(*): new mainPage

---
+++ components/AboutMe/index.tsx
@@ -0,0 +1,7 @@
+export const AboutMe = () => (
+  <section>
+    <h1 className="headline headline--main">Привет! Меня зовут Татьяна.</h1>
+    <p className="body">Я занимаюсь веб-разработкой с 2016 года.</p>
+    <p className="body">Здесь пишу про себя, технологии и книжки.</p>
+  </section>
+);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,17 @@
-import ArticlesList from '@components/ArticlesList';
 import Layout from '@components/Layout';
-import MainArticle from '@components/MainArticle';
 
 import { getAllItems } from '../lib/utils';
+import { SmallArticle } from '../components/Article/small-article';
+import { HCard } from '../components/Partials/microformats/h-card';
+import { MDXRemote } from 'next-mdx-remote';
+import { join } from 'path';
+import matter from 'gray-matter';
+import { serialize } from 'next-mdx-remote/serialize';
+import remarkGfm from 'remark-gfm';
+import fs from 'fs';
 
-export default function Post({ posts }) {
-  const mainArticle = posts[0];
-  const latestArticles = posts.slice(1);
+export default function Post({ posts, mdxSource }) {
+  const latestArticles = posts.slice(0, 5);
 
   return (
     <Layout
@@ -15,42 +20,38 @@ export default function Post({ posts }) {
       image="/title-image.png"
       url="https://yepstepz.io"
     >
-      <section className="main-article">
-        <div>
-          <MainArticle {...mainArticle} />
+      <div className="page__content h-entry">
+        <HCard isAuthor={true} visible={false} />
+        <h1 className="p-name">Привет! Меня зовут Татьяна.</h1>
+        <div className="e-content body">
+          <MDXRemote {...mdxSource} />
         </div>
-      </section>
-      {latestArticles.length !== 0 && (
-        <section className="latest-articles">
-          <div>
-            <div className="block-theme inner--sm">
-              <h2 className="headline headline--second">
-                Что почитать <span className="text text--violet">ещё</span>:
-              </h2>
-            </div>
-            <ArticlesList posts={latestArticles} />
-          </div>
-        </section>
-      )}
+      </div>
+      <ul className="m0 p0">
+        {latestArticles.map(({ slug, date, title }) => {
+          return <SmallArticle slug={slug} date={date} title={title} as="li" />;
+        })}
+      </ul>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const posts = getAllItems([
-    'title',
-    'description',
-    'slug',
-    'content',
-    'image',
-    'blurImage',
-    'tags',
-    'date',
-    'imageAlign',
-    'imageWidth',
-  ]);
+  const markdownWithMeta = fs.readFileSync(
+    join(process.cwd(), 'mdx-landings', 'main.mdx'),
+    'utf-8'
+  );
+  const { data: frontMatter, content } = matter(markdownWithMeta);
+
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+    },
+  });
+
+  const posts = getAllItems(['title', 'date', 'slug']);
 
   return {
-    props: { posts },
+    props: { posts, mdxSource },
   };
 }
